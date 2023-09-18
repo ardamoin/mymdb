1393d635be8e707292943cee06d220eec2da4293
ardamoin <ardamoin2@gmail.com>
2023-09-17 21:20:24 -0400

    Set up environment variable for connecting to Railway MySQL database.

--- server/db.js
+++ server/db.js
@@ -5,12 +5,6 @@ dotenv.config({
   path: "./.env",
 });
 
-const db = mysql.createConnection({
-  host: process.env.DATABASE_HOST, //put ip address for mysql server when in production
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-  port: 8889,
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 module.exports = db;
